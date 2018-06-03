5847f89c7e47078b4fa27911c26375762a3fdfcf
Carlos R Pantoja Caldas <carlosrpcaldas@hotmail.com>
2018-06-03 18:55:51 -0300

    Splashi screen done

--- app/src/main/java/carloscaldas/fiap/com/br/azurecatalog/views/SplashActivity.kt
+++ app/src/main/java/carloscaldas/fiap/com/br/azurecatalog/views/SplashActivity.kt
@@ -1,13 +1,45 @@
 package carloscaldas.fiap.com.br.azurecatalog.views
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import carloscaldas.fiap.com.br.azurecatalog.R
 
 class SplashActivity : AppCompatActivity() {
 
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 4000 //4 seconds
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        //Initialize the Handler
+        mDelayHandler = Handler()
+
+        //Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+
+    }
+
+
+    internal val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    public override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
     }
 }
